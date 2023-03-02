90b3fbc10a7657a34a9f57688af4930048bfcd72
wildbober228 <irina20000@tut.by>
2023-03-02 14:39:16 +0300

    [fix] fix button error

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button {...props} className={'button ' + props.ClassName}></button>
+        <button {...props} className={'button ' + props.ClassName}/>
     );
 };
 
