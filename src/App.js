fe472ec8b31a4520adac6e189570d9212ef19bd5
wildbober228 <irina20000@tut.by>
2023-03-02 14:39:53 +0300

    [fix] fix button error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-        <button onClick={onToggleButton}></button>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
