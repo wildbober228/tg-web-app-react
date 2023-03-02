c557d765046e5647b323b22b07f04232d2123a31
wildbober228 <irina20000@tut.by>
2023-03-02 18:14:21 +0300

    [fix] fix memo bug

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    },[])
+    },[onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
